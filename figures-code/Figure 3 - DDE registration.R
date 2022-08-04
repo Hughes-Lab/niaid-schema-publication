aad83e293344563e4e9a737004ae3f3fc0669c41
flaneuse <flaneuse@users.noreply.github.com>
2022-08-04 16:01:48 -0700

    Update Figure 3 - DDE registration.R

--- figures-code/Figure 3 - DDE registration.R
+++ figures-code/Figure 3 - DDE registration.R
@@ -5,27 +5,35 @@ library(extrafont)
 extrafont::loadfonts()
 
 
-# url = "https://api.data.niaid.nih.gov/v1/query?q=includedInDataCatalog.name:%22Data%20Discovery%20Engine%22&size=1000"
+# Fetch the metadata from the Data Discovery Engine -----------------------
+url_nde = "https://api.data.niaid.nih.gov/v1/query?q=includedInDataCatalog.name:%22Data%20Discovery%20Engine%22&size=1000"
 url = "https://discovery.biothings.io/api/dataset/query?q=_meta.guide:(%22/guide/niaid%22%20OR%20%22/guide/niaid/ComputationalTool%22)&size=1000"
 
 resp = httr::GET(url)
+resp_nde = httr::GET(url_nde)
 
 if(resp$status_code == 200) {
   results = jsonlite::fromJSON(content(resp, as="text"))
   df = results$hits
 }
 
+# The NIAID Data Ecosystem harvests metadata from the DDE and does some minor cleanup of measurementTechniques/pathogens
+if(resp_nde$status_code == 200) {
+  results_nde = jsonlite::fromJSON(content(resp_nde, as="text"))
+  nde = results_nde$hits
+}
+
 # Clean up outdated type info
 df = df %>% mutate(category = str_replace_all(`@type`, "niaid:", ""),
-       category = str_replace_all(category, "Niaid", "")) 
+                   category = str_replace_all(category, "Niaid", ""))
 
 # Donut chart -------------------------------------------------------------
 donutColors = c("#e05e8f", "#32142d")
 
-df %>% 
-  count(category) %>% 
-  mutate(ymax = sum(n), 
-         ymin = n  ) %>% 
+df %>%
+  count(category) %>%
+  mutate(ymax = sum(n),
+         ymin = n  ) %>%
   ggplot(aes(x = 2.7, y = n, fill = category)) +
   geom_col() +
   annotate(geom = "text", x = 1, y = 0, label = str_c(df %>% count(), " total"), family = "Lato", size = 10) +
@@ -42,11 +50,11 @@ df %>%
 getName = function(x) {
   if(length(x) > 0 && x != "NULL") {
     if("name" %in% names(x)) {
-      source_names = x$name  
+      source_names = x$name
     } else {
       source_names = x
     }
-    
+
     if(length(source_names) > 1) {
       return(paste(source_names, collapse = "/"))
     } else {
@@ -57,21 +65,21 @@ getName = function(x) {
   }
 }
 
-sources = df %>% 
+sources = df %>%
   mutate(source_str = sapply(sdPublisher, function(x) getName(x)),
          source_str = str_replace_all(source_str, " and ", "/"),
          source = str_split(source_str, "/"))
 
 
-sources2 = sources %>%   
-  unnest(cols = source) %>% 
-  mutate(source = ifelse(str_trim(source) == "GEO (pprivate)", "GEO", source)) %>% 
+sources2 = sources %>%
+  unnest(cols = source) %>%
+  mutate(source = ifelse(str_trim(source) == "GEO (pprivate)", "GEO", source)) %>%
   count(category, source) %>%
   arrange(desc(n))
 
 sources2$source = factor(sources2$source, sources2 %>% pull(source) %>% unique() %>% rev())
 
-ggplot(sources2, aes(x = source, y = n)) + 
+ggplot(sources2, aes(x = source, y = n)) +
   geom_col() +
   coord_flip() +
   facet_wrap(~category, ncol=1) +
@@ -81,18 +89,39 @@ ggplot(sources2, aes(x = source, y = n)) +
         axis.title = element_blank(),
         panel.grid.major.y = element_blank(),
         legend.position = "none")
-  
+
 
 # pathogens ---------------------------------------------------------
-pathogens = df %>% mutate(pathogens = sapply(infectiousAgent, function(x) x$name)) %>% 
+pathogens = df %>% mutate(pathogens = sapply(infectiousAgent, function(x) x$name)) %>%
   unnest(cols = pathogens)
 
-pathogens %>% count(pathogens) %>% 
+pathogens %>% count(pathogens) %>%
   arrange(desc(n))
 
 # measurement techniques ---------------------------------------------------------
-pathogens = df %>% mutate(pathogens = sapply(measurementTechnique, function(x) getName(x))) %>% 
-  unnest(cols = pathogens)
+techniques = nde %>% mutate(
+  techniques_str = sapply(measurementTechnique, function(x) getName(x)),
+  techniques = str_split(techniques_str, "/")) %>%
+  unnest(cols = techniques) %>%
+  mutate(techniques = str_trim(techniques))
 
-pathogens %>% count(pathogens) %>% 
+
+
+# Count number of measurement techniques. A dataset or tool can have multiple techiques
+techniques_count = techniques %>%
+  # lump into other if not frequent
+  mutate(techniques_grp = fct_lump_n(techniques, 20)) %>%
+  count(techniques_grp) %>%
   arrange(desc(n))
+
+techniques_count$techniques_grp = factor(techniques_count$techniques_grp, techniques_count$techniques_grp %>%  rev())
+
+ggplot(techniques_count, aes(x = techniques_grp, y = n)) +
+  geom_col() +
+  coord_flip() +
+  ggtitle("Measurement technique in catalogued datasets and computational tools") +
+  theme_minimal() +
+  theme(text = element_text(family = "Lato", size = 18),
+        axis.title = element_blank(),
+        panel.grid.major.y = element_blank(),
+        legend.position = "none")
